02239153a322eaf0e43c81e0525bfb2be31c7a19
Phantom <phantom@phantom-games.com>
2023-03-26 14:21:01 -0700

    keep prologue as it's own thing instead of grouping with chapter 1

--- maps/graph_edges/streamlined_chapters/edges_ch1.py
+++ maps/graph_edges/streamlined_chapters/edges_ch1.py
@@ -4,16 +4,16 @@ Chapter 1 is not required for Streamlined Chapters.
 """
 streamlined_edges_ch1_add = [
     # Gate District Exit Left -> Toad Town Entrance Exit Right
-    {"from": {"map": "MAC_00", "id": 0}, "to": {"map": "KMR_10", "id": 1},
-     "reqs": [["YOUWIN"]]},
+    #{"from": {"map": "MAC_00", "id": 0}, "to": {"map": "KMR_10", "id": 1},
+    # "reqs": [["YOUWIN"]]},
 
     # Plaza District Exit Right -> Pleasant Path Entry Exit Left
     {"from": {"map": "MAC_01", "id": 1}, "to": {"map": "NOK_11", "id": 0},
      "reqs": [["YOUWIN"]]},
 
     # Warp Zone 1 (B1) Blue Warp Pipe (Right) -> Goomba Village Blue Warp Pipe
-    {"from": {"map": "TIK_01", "id": 2}, "to": {"map": "KMR_02", "id": 3},
-     "reqs": [["YOUWIN"]]},
+    #{"from": {"map": "TIK_01", "id": 2}, "to": {"map": "KMR_02", "id": 3},
+    # "reqs": [["YOUWIN"]]},
 
     # Warp Zone 1 (B1) Blue Warp Pipe (Center) -> Koopa Village 2 Blue Pipe
     {"from": {"map": "TIK_01", "id": 3}, "to": {"map": "NOK_02", "id": 2},
@@ -23,13 +23,13 @@ streamlined_edges_ch1_add = [
 
 streamlined_edges_ch1_remove = [
     # Gate District Exit Left -> Toad Town Entrance Exit Right
-    {"from": {"map": "MAC_00", "id": 0}, "to": {"map": "KMR_10", "id": 1}, "reqs": []},
+    #{"from": {"map": "MAC_00", "id": 0}, "to": {"map": "KMR_10", "id": 1}, "reqs": []},
 
     # Plaza District Exit Right -> Pleasant Path Entry Exit Left
     {"from": {"map": "MAC_01", "id": 1}, "to": {"map": "NOK_11", "id": 0}, "reqs": []},
 
     # Warp Zone 1 (B1) Blue Warp Pipe (Right) -> Goomba Village Blue Warp Pipe
-    {"from": {"map": "TIK_01", "id": 2}, "to": {"map": "KMR_02", "id": 3}, "reqs": []},
+    #{"from": {"map": "TIK_01", "id": 2}, "to": {"map": "KMR_02", "id": 3}, "reqs": []},
 
     # Warp Zone 1 (B1) Blue Warp Pipe (Center) -> Koopa Village 2 Blue Pipe
     {"from": {"map": "TIK_01", "id": 3}, "to": {"map": "NOK_02", "id": 2}, "reqs": []},
